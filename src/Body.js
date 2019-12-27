e07479735ce11015c28aab2084def0288647b492
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-12-27 21:28:30 +0000

    testing out my class and functional component

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Body(){
+return(
+    <div>
+
+        this is a functional component
+    </div>
+)
+}
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react"
+
+class Header extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+            this is a man
+            </div>
+         );
+    }
+}
+ 
+export default Header;
