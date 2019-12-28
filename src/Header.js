e701a51f703fe73f26424293e547bf5784a69713
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-12-28 00:40:27 +0000

    working on the project

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,28 @@
 import React,{Component} from "react"
 
 class Header extends Component {
-    state = {  }
+    state = { 
+   monster:[
+       {name:"blessed",
+        id:1},
+       {name:"Emoefe",
+       id:2},
+       {name:"junior",
+       id:3},
+       {name:"havard",
+       id:4}
+
+   ]
+     }
+     componentDidMount(){
+         fetch("https://jsonplaceholder.typicode.com/users")
+         .then(response=>response.json())
+         .then(issue=>console.log(issue))
+     }
     render() { 
         return ( 
-            <div>
-            this is a man
+            <div><h3>thi is</h3>
+                {this.state.monster.map(individual=><h2 key={this.state.monster.id}>{individual.name}</h2>)}
             </div>
          );
     }
