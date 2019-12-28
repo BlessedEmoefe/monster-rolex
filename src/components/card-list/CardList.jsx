8d574899d1ace38de7cc57fa795be3298102eadc
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-12-28 02:50:00 +0000

    working on the project

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -5,7 +5,7 @@ import Card from "../card/Card"
 const CardList = (props) =>{
     return(
         <div className="cardlist">
-              {props.monster.map(individual=><Card key={props.monster.id} monster={individual} /> )}
+              {props.monster.map(individual=><Card key={props.monster.id} individual={individual}/>)}
         </div>
     )
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,9 @@
 import React from "react"
+import "./card.style.css"
 
 const Card = (props) =>{
 return(
-    <div>
+    <div className="card-container">
         <h2 >{props.individual.name}</h2>
     </div>
 )
